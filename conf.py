a2751f729c98577ea464bedd2eadbfd006eb3e2c
grelade <jacekgrela@gmail.com>
2019-05-14 20:01:09 +0100

    init commit

---
+++ conf.py
@@ -0,0 +1,25 @@
+# world size
+xsize = 12
+ysize = 6
+
+# GUI scalings
+scalex=100
+scaley=100
+
+# start and end states
+statestart = [0,int((ysize-1)/2)]
+stateend = [xsize-3,int((ysize-1)/2)]
+
+# a cap for drawing the trajectory
+drawingcap = 2000
+
+# averaging window size
+windowsize = 50
+
+# default values of parameters
+defaultalpha = 0.1
+defaultnoepisodes = 1000
+defaultgamma = 1
+defaultepsilon = 0.1
+defaultinitq = 0
+defaultlambda = 0.1
